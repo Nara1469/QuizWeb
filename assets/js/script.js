490b00d86092c3ea164578bca0e34a59ee340323
Nara Davaasuren <naraamtm@gmail.com>
2022-01-21 11:36:43 -0700

    Added screen changing procedures

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,46 @@
 // variables
 const questionArray = [
     {
+      question: "String value must be enclosed within ________ when being assigned to variables.",
+      answers: [
+        "commas",
+        "curly brackets",
+        "quotes",
+        "parenthesis"
+      ],
+      correctAnswer: "quotes"
+    },
+    {
+      question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+      answers: [
+        "Javascript",
+        "Terminal/Bash",
+        "for loops",
+        "console.log"
+      ],
+      correctAnswer: "console.log"
+    },
+    {
+      question: "Commonly used data types DO NOT include:",
+      answers: [
+        "strings",
+        "booleans",
+        "alerts",
+        "numbers"
+      ],
+      correctAnswer: "alerts"
+    },
+    {
+      question: "The condition is an if/else statement is enclosed within ______.",
+      answers: [
+        "square brackets",
+        "curly brackets",
+        "quotes",
+        "parenthesis"
+      ],
+      correctAnswer: "curly brackets"
+    },
+    {
       question: "Who invented JavaScript?",
       answers: [
         "Douglas Crockford",
@@ -8,17 +48,7 @@ const questionArray = [
         "Tim Berners-Lee",
         "Brendan Eich"
       ],
-      correctAnswer: 2
-    },
-    {
-      question: "Which one of these is a JavaScript package manager?",
-      answers: [
-        "npm",
-        "Node.js",
-        "TypeScript",
-        "npm"
-      ],
-      correctAnswer: "c"
+      correctAnswer: "Brendan Eich"
     },
     {
       question: "Which tool can you use to ensure code quality?",
@@ -28,11 +58,10 @@ const questionArray = [
         "RequireJS",
         "ESLint"
       ],
-      correctAnswer: 3
+      correctAnswer: "ESLint"
     }
   ];
 
-const currentScore = document.querySelector(".current-score");
 const timer = document.getElementById("timer");
 
 const startScreen = document.querySelector("#start-screen");
@@ -44,8 +73,10 @@ const firstAnswer= document.getElementById("li1");
 const secondAnswer= document.getElementById("li2");
 const thirdAnswer= document.getElementById("li3");
 const fourthAnswer= document.getElementById("li4");
+const showResult= document.getElementById("result");
 
 const endScreen = document.querySelector("#end-screen");
+const currentScore = document.querySelector("#current-score");
 const studentName= document.getElementById("student-name");
 const submitButton = document.getElementById("submit");
 
@@ -54,7 +85,17 @@ const studentList = document.querySelector("#result-list");
 const againButton = document.getElementById("start-again");
 const clearButton = document.getElementById("clear-scores");
 
-// functions
+let students = {
+  initial: "",
+  score: 0
+}
+let timerCount = 100;
+let realScore = 0;
+let isCorrect = false;
+let currentPosition = 0;
+
+// functions for Screen changes
+
 function callStartScreen() {
     // event.preventDefault();
     startScreen.classList.remove("hide");
@@ -66,32 +107,59 @@ function callTestScreen() {
     // event.preventDefault();
     testScreen.classList.remove("hide");
     startScreen.classList.add("hide");
-    
+    timerCount = 100;
+    startTimer();
+    testTaking();
 }
 
 function callEndScreen(event) {
     // event.preventDefault();
     endScreen.classList.remove("hide");
     testScreen.classList.add("hide");
-    
+    submitButton.addEventListener("click", getName);   
 }
 
 function callResultScreen(event) {
     // event.preventDefault();
     resultScreen.classList.remove("hide");
     endScreen.classList.add("hide");
-    
+    renderList();
+    againButton.addEventListener("click", callStartScreen);
+    clearButton.addEventListener("click", clearStorage);
 }
 
+// Testing Timer
 
+function startTimer() {
+  timerClock = setInterval(function() {
+    timerCount--;
+    timer.textContent = timerCount;
+    // if (timerCount >= 0){
+    //   // Tests if correct answer is chosen
+    //   if (isCorrect === false) {
+    //     //Clears interval and stops timer
+    //     timerCount = timerCount - 10;
+    //   }
+    // }
+    if (timerCount <= 0) {
+      //Clears interval and stops timer
+       clearInterval(timerClock);
+       timerCount = 0;
+       realScore = 0;
+       callEndScreen();
+    }
+  }, 1000);
+}
 
+// Taking test taker's info in localStorage
 function getName(event) {
     event.preventDefault();
-
-    const tempName = studentName.value.trim();
+    currentScore.textContent = realScore;
+    let tempName = studentName.value.trim(); // tempName.toUppercase !!!
     if (tempName){
-        students.push(tempName);
-        localStorage.setItem("names", JSON.stringify(students));
+        localStorage.setItem("names", JSON.stringify(tempName));
+        localStorage.setItem("scores", JSON.stringify(realScore));
+        callResultScreen();
     } else {
         alert("Name required. Try again.");
     }
@@ -109,31 +177,65 @@ function init() {
 }
 
 function renderList() {
-    for (let index = 0; index < students.length; index++) {
-        const student = students[index];
-        const newLi = document.createElement("li");
-        newLi.textContent=student;
-        studentList.appendChild(newLi);
-    }
+    let tempName = JSON.parse(localStorage.getItem("names"));
+    let tempScore = JSON.parse(localStorage.getItem("scores"));
+    const newLi = document.createElement("li");
+    newLi.textContent = tempName + ": " + tempScore;
+    studentList.appendChild(newLi);
+    // for (let index = 0; index < students.length; index++) {
+    //   const student = students[index];
+    //   const newLi = document.createElement("li");
+    //   newLi.textContent = tempName + ": " + tempScore;
+    //   studentList.appendChild(newLi);
+    // }
+}
+
+function clearStorage(event) {
+    event.preventDefault();
+    localStorage.clear();
 }
 
 function randomizeList(event) {
     event.preventDefault();
 
     let randoList = [];
-    for (let i = 0; i < questions.length; i++) {
+    for (let i = 0; i < questionArray.length; i++) {
         randoList[i] = Math.floor(Math.random() * questions.length);
     }
-    
+}
+  
+function testTaking() {
+  let i = 0; 
+
+  // until all questions answered or timer is done
+  while (i < questionArray.length && timerCount > 0) { 
+    questionId.textContent = questionArray[i].question;
+    firstAnswer.textContent = questionArray[i].answers[0];
+    secondAnswer.textContent = questionArray[i].answers[1];
+    thirdAnswer.textContent = questionArray[i].answers[2];
+    fourthAnswer.textContent = questionArray[i].answers[3];
+    document.querySelector("li").addEventListener("click", checkAnswer(i));
+    if (isCorrect) {
+      i++;
+    }
+  }
+  
 }
 
-// logic  
-function checkAnswer(params) {
-    questionId.textContent = questionArray[0].question;
+function checkAnswer(position) {
+    showResult.classList.remove("hide");
+    console.log(position);
+    if (document.querySelector("li").textContent === questionArray[position].correctAnswer) {
+        isCorrect = true;
+        showResult.textContent = "Right";
+    } else {
+        isCorrect = false;
+        showResult.textContent = "Wrong";
+        timerCount = timerCount - 10;
+        if (timerCount <= 0) {
+            timerCount = 0;
+        }
+    }
 }
-
-
-
-document.querySelector("ol").addEventListener("click", checkAnswer);
 
 init();
